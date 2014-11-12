48332f1c3a502492167508f6ce0e916cf2afe923
Enrico Bragante <enrico@ebiteng.com>
2014-11-12 06:45:42 +0100

    clip selection

--- mpkMiniMkII/controls/TrackControl.js
+++ mpkMiniMkII/controls/TrackControl.js
@@ -10,12 +10,12 @@ co.nri.tk.TrackControl = function TrackControl(registry, host, device, transport
 	this.init = function() {
 		
 		var trackBank;
+		self.slots = host.createCursorTrack(8,8).getClipLauncherSlots();
+		self.slots.setIndication(false)
 		
 		registry.setHandler('tk.nextTrack', function(direction, status, data1, val) {
 			if(val === 0) return;
 			track.selectNext();
-			track.returnToArrangement();
-			trackBank = host.createMainTrackBank(1, 0, 0);
 		});
 		
 		registry.setHandler('tk.prevTrack', function(direction, status, data1, val) {
@@ -28,14 +28,15 @@ co.nri.tk.TrackControl = function TrackControl(registry, host, device, transport
 		registry.setHandler('tk.nextSlot', function(direction, status, data1, val) {
 			if(val === 0) return;
 			//track.getClipLauncherSlots().select(++self.selectedSlot);#
-			println(trackBank.getTrack(0).getClipLauncherSlots());
+			//println(trackBank.getTrack(0).getClipLauncherSlots());
+			self.slots.select(++self.selectedSlot);
 		});
 		
 		registry.setHandler('tk.prevSlot', function(direction, status, data1, val) {
 			if(val === 0) return;
-			//if(self.selectedSlot === 0) return;
+			if(self.selectedSlot === 0) return;
 			
-			//track.getClipLauncherSlots().select(--self.selectedSlot);
+			self.slots.select(--self.selectedSlot);
 		});
 		
 	};
